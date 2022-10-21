7e91133722e726007d80aecdba8747178a317e00
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-20 22:00:16 -0300

    style: Home page's mains style

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,17 @@
+import Header from "../../Components/Header"
+import Page from './style'
 
 const Home = () => {
     return (
-    <p>ola</p>
+        <div>
+            <Header />
+            <Page>
+                <div className="page__text">
+                    <h1>Magic Book</h1>
+                    <p>A guide for wizards and muggles!</p>
+                </div>
+            </Page>
+        </div>
     )
 }
 
---
+++ src/Pages/Home/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Page = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400&display=swap');
+
+    margin: 0 10vw; 
+    color: #fff;
+    height:100vh;
+    background-color: rgb(217,217,217,.55);
+
+    .page__text{
+        padding-top: .8rem;
+        text-align:center;
+        font-family: 'Dancing Script', cursive;
+        font-size: 2rem
+    }
+    
+`
+
+export default Page
