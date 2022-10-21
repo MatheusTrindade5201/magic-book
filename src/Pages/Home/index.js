38567ec6de59dd347aa4b639aabe97d72e68b3e0
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-21 20:59:02 -0300

    feat: implementação de lógica de consumo da API na Home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
 import Header from "../../Components/Header"
+import api from "../../Services/api";
 import Page from './style'
 
 const Home = () => {
+
+    const [list, setList] = useState(false);
+
+    
+
+    useEffect(()=>{
+        api.get('/characters')
+        .then((response)=> setList(response.data));
+        ;
+    },[])
+
+
     return (
         <div>
             <Header />
