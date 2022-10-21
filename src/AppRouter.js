a898f6b696b73a9b59bcdbf8c99c10e2b0c65156
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-20 22:04:23 -0300

    refactor: styled component tag removed

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-import { GlobalStyle } from "./Styles/global";
 
 const AppRouter = () => {
     return (
@@ -9,7 +8,6 @@ const AppRouter = () => {
         <Routes>
             <Route element={<Home />} path='/' />
         </Routes>
-        <GlobalStyle imgUrl={'./assets/image/hogwarts-b.jpg'}/>
         </BrowserRouter>
     )
 }
