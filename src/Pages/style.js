398ffe8c6df72e41f108b05b13890f3f59c8541b
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-24 20:47:45 -0300

    refactor: Retruturing the folders to use a global style for the pages

---
+++ src/Pages/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Page = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400&display=swap');
+
+    margin: 0 10vw; 
+    color: #fff;
+    height:100%;
+    background-color: ${props => props.theme.settings.bg__color};
+
+    .page__text{
+        padding-top: .8rem;
+        text-align:center;
+        font-family: 'Dancing Script', cursive;
+        font-size: 2rem;
+        color: ${props => props.theme.settings.title__color}
+    }
+    
+    .list{
+        margin: 2rem 2vw 0 2vw;
+        padding-bottom: 5rem;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2rem;
+        justify-content: space-around
+    }
+    
+`
+
+export default Page
