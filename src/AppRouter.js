d08ab6374f4ded248736417532f46505458eaf3a
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-24 20:48:40 -0300

    feat: Creating a page that presents only the students

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {  Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Students from "./Pages/Students";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route element={<Home />} path='/' />
+            <Route element={<Students />} path='/students' />
         </Routes>
     )
 }
