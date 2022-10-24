241e3f43c2a4e2b61bb1b1d7adcf86e26e9ce737
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-24 20:55:29 -0300

    feat: Creating a page that presents only the staff

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {  Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Staff from "./Pages/Staff";
 import Students from "./Pages/Students";
 
 const AppRouter = () => {
@@ -8,6 +9,7 @@ const AppRouter = () => {
         <Routes>
             <Route element={<Home />} path='/' />
             <Route element={<Students />} path='/students' />
+            <Route element={<Staff />} path='/staff' />
         </Routes>
     )
 }
---
+++ src/Pages/Staff/index.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import Card from "../../Components/Card";
+import Search from "../../Components/Search";
+import api from "../../Services/api";
+import Page from '../style'
+
+const Staff = () => {
+
+    const [list, setList] = useState(false);
+    const [characterName, setCharacterName] = useState('')
+
+    
+
+    useEffect(()=>{
+        api.get('/characters/staff')
+        .then((response)=> setList(response.data));
+        console.log(list);
+    },[list])
+
+    if(!list){
+        return(
+            <div>
+            <Page>
+                <div className="page__text">
+                    <h1>Magic Book</h1>
+                    <p>A guide for wizards and muggles!</p>
+                    <Search />
+                    <p>Loading...</p>
+                </div>
+                
+            </Page>
+        </div>
+        )
+    }
+    return (
+        <div>
+            <Page>
+                <div className="page__text">
+                    <h1>Magic Book</h1>
+                    <p>A guide for wizards and muggles!</p>
+                    <Search placeholder={'Accio Character...'}
+                    onTyped={value => setCharacterName(value)}/>
+                </div>
+                <div className="list">
+                    {list.filter(character => character.name.toUpperCase().includes(characterName.toUpperCase())).map(character => <Card
+                    image={character.image.length > 0 ? character.image : require("../../assets/image/No-Image-Placeholder.svg.png")}
+                    name={character.name}
+                    icon={character.house === "Gryffindor" ? require('../../assets/image/Gryffindor.png'): character.house === "Slytherin" ? require('../../assets/image/Slytherin.png') : character.house === "Hufflepuff" ? require('../../assets/image/Hufflepuff.png') : character.house === "Ravenclaw" ? require('../../assets/image/Ravenclaw.png') : ''}
+                    ancestry={(character.ancestry).charAt(0).toUpperCase()+(character.ancestry).slice(1)}
+                    species={(character.species).charAt(0).toUpperCase()+(character.species).slice(1)}
+                    house={character.house}
+                    patronus={(character.patronus).charAt(0).toUpperCase()+(character.patronus).slice(1)}
+                    wood={(character.wand.wood).charAt(0).toUpperCase()+(character.wand.wood).slice(1)}
+                    core={(character.wand.core).charAt(0).toUpperCase()+(character.wand.core).slice(1)}
+                    length={character.wand.length}
+                />)}
+                </div>
+            </Page>
+        </div>
+    )
+}
+
+export default Staff
