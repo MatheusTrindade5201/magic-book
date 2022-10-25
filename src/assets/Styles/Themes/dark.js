4979704f0b0a4f5b5bf750c4aeb82545f74dec8b
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-24 21:35:57 -0300

    feat: defining theme colors for spell cards

--- src/assets/Styles/Themes/dark.js
+++ src/assets/Styles/Themes/dark.js
@@ -4,6 +4,7 @@ export default {
         bg__image: require('../../image/hogwarts-b.jpg'),
         bg__color: 'rgb(217,217,217,.55)',
         text: '#fff',
-        title__color: 'rgb(0, 0, 0,.6)'
+        title__color: 'rgb(0, 0, 0,.6)',
+        spell_card: 'rgb(0, 0, 0,.8)'
     }
 }
--- src/assets/Styles/Themes/light.js
+++ src/assets/Styles/Themes/light.js
@@ -4,6 +4,7 @@ export default {
         bg__image: require('../../image/hogwarts-b-light.jpg'),
         bg__color: 'rgb(0,0,0,.55)',
         text: '#fff',
-        title__color: '#fff'
+        title__color: '#fff',
+        spell_card: 'rgb(255, 228, 179,.3)'
     }
 }
