71da8b3a7a1c8b5990b6b6deee8356c09070e40c
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-26 19:59:19 -0300

    fix: removing react icon package due some bugs

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import logo from '../../assets/image/logo.png'
 import { HeaderPage } from './style'
-import { FaBars } from 'react-icons/fa'
 
 const Header = (props) => {
     return (
@@ -14,7 +13,6 @@ const Header = (props) => {
                 <NavLink className='NavLink' to='/staff'>Staff</NavLink>
                 <NavLink className='NavLink' to='/spell-book'>Spell Book</NavLink>
                 </nav>
-                <FaBars onClick={props.menuOpener} className='mobile-button'/>
                 {/* <button  onClick={props.menuOpener} className='mobile-button' /> */}
                 <button onClick={props.toggleTheme}>{props.themeName}</button>
             </div> 
