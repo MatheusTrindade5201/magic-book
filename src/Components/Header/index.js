22734af2861c1383516d199469735ee40a64df3a
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-26 19:47:24 -0300

    feat: changing fontAwesome package for React Icons

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { NavLink } from 'react-router-dom'
 import logo from '../../assets/image/logo.png'
 import { HeaderPage } from './style'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FaBars } from 'react-icons/fa'
 
 const Header = (props) => {
     return (
@@ -15,7 +14,8 @@ const Header = (props) => {
                 <NavLink className='NavLink' to='/staff'>Staff</NavLink>
                 <NavLink className='NavLink' to='/spell-book'>Spell Book</NavLink>
                 </nav>
-                <FontAwesomeIcon onClick={props.menuOpener} className='mobile-button' icon={faBars}/>
+                <FaBars onClick={props.menuOpener} className='mobile-button'/>
+                {/* <button  onClick={props.menuOpener} className='mobile-button' /> */}
                 <button onClick={props.toggleTheme}>{props.themeName}</button>
             </div> 
                 <nav className={props.menuMobile + 'mobile'}>
